8db3459f7478e351d96ec178251b078ebaa45a8f
Maxime Della-Luce <maxime.della.luce@gmail.com>
2020-04-27 19:54:28 +0200

    Mock smbus to avoid calls to hardware during tests

--- tests/mygreenhouse/hardware/test_adc.py
+++ tests/mygreenhouse/hardware/test_adc.py
@@ -1,11 +1,13 @@
 import unittest
+from unittest.mock import patch
 
 from mygreenhouse.hardware.adc import PCF8591
 from mygreenhouse.hardware.i2c_addresses import I2CAddresses
 
 
 class PCF8591Tests(unittest.TestCase):
-    def test_correct_I2C_address_used_for_PCF8591(self):
+    @patch('smbus.SMBus')
+    def test_correct_I2C_address_used_for_PCF8591(self, mock_smbus):
         self.assertEqual(I2CAddresses.PCF8591, PCF8591().address)
 
 
