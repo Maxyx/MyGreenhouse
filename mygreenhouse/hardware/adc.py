75526501fa774291625990fc09d89dd07c16b6a9
Maxime Della-Luce <maxime.della.luce@gmail.com>
2020-05-03 17:49:21 +0200

    Add possibility to read from specific ADC channel

--- mygreenhouse/hardware/adc.py
+++ mygreenhouse/hardware/adc.py
@@ -4,4 +4,8 @@ from mygreenhouse.hardware.i2cdevice import I2CDevice
 
 class PCF8591(I2CDevice):
     def __init__(self, communication_handler):
-        super().__init__(I2CAddresses.PCF8591, communication_handler)
+        super().__init__(I2CAddresses.PCF8591.value, communication_handler)
+        self.standard_command = 0x40
+
+    def analog_read_of_channel(self, channel):
+        return self.communication_handler.read_byte_from_register(self.address, self.standard_command + channel)
--- tests/mygreenhouse/hardware/test_adc.py
+++ tests/mygreenhouse/hardware/test_adc.py
@@ -7,7 +7,16 @@ from mygreenhouse.hardware.i2c_addresses import I2CAddresses
 
 class PCF8591Tests(unittest.TestCase):
     def test_correct_I2C_address_used_for_PCF8591(self):
-        self.assertEqual(I2CAddresses.PCF8591, PCF8591(MagicMock()).address)
+        self.assertEqual(I2CAddresses.PCF8591.value, PCF8591(MagicMock()).address)
+
+    def test_analog_read_from_channel_reads_correct_register(self):
+        mock_i2c = MagicMock()
+        adc = PCF8591(mock_i2c)
+        channels_registers = [0x40, 0x41, 0x42, 0x43]
+        for i in range(4):
+            with self.subTest(msg='Correct register read for channel {}'.format(i), i=i):
+                adc.analog_read_of_channel(i)
+                mock_i2c.read_byte_from_register.assert_called_with(adc.address, channels_registers[i])
 
 
 if __name__ == '__main__':
